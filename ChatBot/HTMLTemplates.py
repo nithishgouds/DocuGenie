517dced05a629e2abd016516987c53658470a052
nithishgouds <srinithishgoudsuragouni@gmail.com>
2025-07-01 01:11:03 +0530

    Update HTMLTemplates.py

--- ChatBot/HTMLTemplates.py
+++ ChatBot/HTMLTemplates.py
@@ -1,34 +1,38 @@
 css = '''
 <style>
 .chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+    padding: 1.5rem; 
+    border-radius: 0.5rem; 
+    margin-bottom: 1rem; 
+    display: flex;
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #2b313e;
 }
 .chat-message.bot {
-    background-color: #475063
+    background-color: #475063;
 }
 .chat-message .avatar {
-  width: 20%;
+    width: 20%;
 }
 .chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
+    width: 78px;
+    height: 78px;
+    border-radius: 50%;
+    object-fit: cover;
 }
 .chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
+    width: 80%;
+    padding: 0 1.5rem;
+    color: #fff;
 }
+</style>
 '''
 
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://im.rediff.com/movies/2018/sep/14robot1.jpg?w=670&h=900" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://im.rediff.com/movies/2018/sep/14robot1.jpg">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
